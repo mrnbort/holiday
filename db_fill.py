411c02fbc3486a2a7b921f4ac81eaf8c9365f314
marynabort <maryna@bort.name>
2022-04-12 18:49:21 -0500

    auth added

--- db_fill.py
+++ db_fill.py
@@ -1,11 +1,6 @@
-from database import engine, session
 import models
 from sqlalchemy.orm import Session
-from models import Holidays
-import schemas
 import hashlib
-
-# models.Base.metadata.create_all(engine)
 
 
 def holiday_db(db: Session, holiday):
@@ -21,17 +16,3 @@ def holiday_db(db: Session, holiday):
         return 'A new holiday added'
     else:
         return 'Unique constraint violation'
-
-
-# def holiday_db(holidays_tuple):
-#     for holiday in holidays_tuple:
-#         add_holi = Holidays(desc=holiday[0], date=holiday[1])
-#         if session.query(models.Holidays) \
-#                 .filter(models.Holidays.desc == holiday[0]) \
-#                 .filter(models.Holidays.date == holiday[1]).first() is None:
-#             session.add(add_holi)
-#             session.commit()
-#             print('A new holiday added')
-#         else:
-#             print('Unique constraint violation')
-#     session.close()
